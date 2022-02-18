257703c3e32ea159438ed1760314f9dbb47275e0
yooju <janhold@naver.com>
2022-02-18 11:39:12 +0900

    test

---
+++ app_clinic.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/clinic')
+def clinic():
+   return render_template('index_clinic.html')
+
+if __name__ == '__main__':
+   app.run('0.0.0.0',port=5050,debug=True)
