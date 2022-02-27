df4a1d272c1e5a1356511ebda8db434d1cdb72ad
yooju <janhold@naver.com>
2022-02-27 10:40:03 +0900

    4th-bfore change pos

--- static/js/map.js
+++ static/js/map.js
@@ -1,28 +1,30 @@
-var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
-var options = { //지도를 생성할 때 필요한 기본 옵션
-    center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
-    level: 7 //지도의 레벨(확대, 축소 정도)
-};
+    var container = document.getElementById('map');  // 지도를 표시할 div
+    var options = {
+               center: new kakao.maps.LatLng(37.5166119773031, 127.041258693516),  // 지도의 중심좌표
+               level: 7  // 지도 확대 레벨
+    };
+    // 지도를 표시할 div 와 지도 옵션으로 지도 생성
+    var map = new kakao.maps.Map(container, options);
 
-var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+    // 마커생성 추가
+    var markerPosition  = new kakao.maps.LatLng(37.5166119773031, 127.041258693516);
+    var marker = new kakao.maps.Marker({position: markerPosition});
+    marker.setMap(null);
 
-var places = new kakao.maps.services.Places();
-places.setMap(null);
-places.setMap(map);
-
+//////////////////////////////////
 var mapContainer = document.getElementById('map'), // 지도를 표시할 div
     mapOption = {
         center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
         level: 7 // 지도의 확대 레벨
     };
-//////////////////////////////////
-map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
+
+var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
 
 // HTML5의 geolocation으로 사용할 수 있는지 확인합니다
 if (navigator.geolocation) {
 
     // GeoLocation을 이용해서 접속 위치를 얻어옵니다
-    navigator.geolocation.getCurrentPosition(function (position) {
+    navigator.geolocation.getCurrentPosition(function(position) {
 
         var lat = position.coords.latitude, // 위도
             lon = position.coords.longitude; // 경도
@@ -33,7 +35,7 @@ if (navigator.geolocation) {
         // 마커와 인포윈도우를 표시합니다
         displayMarker(locPosition, message);
 
-    });
+      });
 
 } else { // HTML5의 GeoLocation을 사용할 수 없을때 마커 표시 위치와 인포윈도우 내용을 설정합니다
 
@@ -57,8 +59,8 @@ function displayMarker(locPosition, message) {
 
     // 인포윈도우를 생성합니다
     var infowindow = new kakao.maps.InfoWindow({
-        content: iwContent,
-        removable: iwRemoveable
+        content : iwContent,
+        removable : iwRemoveable
     });
 
     // 인포윈도우를 마커위에 표시합니다
@@ -68,3 +70,63 @@ function displayMarker(locPosition, message) {
     map.setCenter(locPosition);
 }
 
+/////////////////////
+// 마커를 클릭하면 장소명을 표출할 인포윈도우 입니다
+var infowindow = new kakao.maps.InfoWindow({zIndex:1});
+
+var mapContainer = document.getElementById('map'), // 지도를 표시할 div
+    mapOption = {
+        center: new kakao.maps.LatLng(37.566826, 126.9786567), // 지도의 중심좌표
+        level: 3 // 지도의 확대 레벨
+    };
+
+// 지도를 생성합니다
+var map = new kakao.maps.Map(mapContainer, mapOption);
+
+// 장소 검색 객체를 생성합니다
+var ps = new kakao.maps.services.Places();
+
+// 키워드로 장소를 검색합니다
+ps.keywordSearch('선별진료소', placesSearchCB);
+ps.keywordSearch('코로나19', placesSearchCB);
+ps.keywordSearch('코로나', placesSearchCB);
+ps.keywordSearch('보건소', placesSearchCB);
+ps.keywordSearch('검사소', placesSearchCB);
+ps.keywordSearch('임시선별검사소', placesSearchCB);
+ps.keywordSearch('코로나19 좋은강안병원 선별진료소', placesSearchCB);
+
+
+// 키워드 검색 완료 시 호출되는 콜백함수 입니다
+function placesSearchCB (data, status, pagination) {
+    if (status === kakao.maps.services.Status.OK) {
+
+        // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
+        // LatLngBounds 객체에 좌표를 추가합니다
+        var bounds = new kakao.maps.LatLngBounds();
+
+        for (var i=0; i<data.length; i++) {
+            displayMarker(data[i]);
+            bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
+        }
+
+        // 검색된 장소 위치를 기준으로 지도 범위를 재설정합니다
+        map.setBounds(bounds);
+    }
+}
+
+// 지도에 마커를 표시하는 함수입니다
+function displayMarker(place) {
+
+    // 마커를 생성하고 지도에 표시합니다
+    var marker = new kakao.maps.Marker({
+        map: map,
+        position: new kakao.maps.LatLng(place.y, place.x)
+    });
+
+    // 마커에 클릭이벤트를 등록합니다
+    kakao.maps.event.addListener(marker, 'click', function() {
+        // 마커를 클릭하면 장소명이 인포윈도우에 표출됩니다
+        infowindow.setContent('<div style="padding:5px;font-size:12px;">' + place.place_name + '</div>');
+        infowindow.open(map, marker);
+    });
+}
