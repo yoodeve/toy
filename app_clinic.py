212e67dfd14c446630de87ea179a88ea5a603ff7
JaejinLee <jaejin1112@gmail.com>
2022-03-01 16:52:15 +0900

    file delete and change

--- app_clinic.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template
-app = Flask(__name__)
-
-@app.route('/clinic')
-def clinic():
-   return render_template('index_clinic.html')
-
-if __name__ == '__main__':
-   app.run('0.0.0.0',port=5050,debug=True)
