f260a6b90416a60a11c3e851114d79cec9e0b466
JaejinLee <jaejin1112@gmail.com>
2022-03-01 16:51:02 +0900

    upload need organize

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ from pymongo import MongoClient
 client = MongoClient('mongodb+srv://copa:copa@cluster0.dmead.mongodb.net/Cluster0?retryWrites=true&w=majority')
 db = client.dbsparta
 
+@app.route('/clinic')
+def clinic():
+    return render_template('index_clinic.html')
+
 @app.route('/check_list')
 def check_list():
     return render_template('index_check.html')
