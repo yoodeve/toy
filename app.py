eb5aa1a0a17fe5b42daec76ef1c7bd586f237cd8
JaejinLee <jaejin1112@gmail.com>
2022-03-03 16:16:33 +0900

    change password

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
 from pymongo import MongoClient
-client = MongoClient('mongodb+srv://copa:copa@cluster0.dmead.mongodb.net/Cluster0?retryWrites=true&w=majority')
+
+client = MongoClient('mongodb+srv://copa:20220303@cluster0.dmead.mongodb.net/Cluster0?retryWrites=true&w=majority')
 db = client.dbsparta
 
 @app.route('/')
@@ -21,11 +22,6 @@ def check_list():
 def check_get():
     symp_list = list(db.check.find({}, {'_id': False}))
     return jsonify({'symps': symp_list})
-
-@app.route("/result", methods=["GET"])
-def result_get():
-    result_list = list(db.result.find({}, {'_id': False}))
-    return jsonify({'results': result_list})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5050, debug=True)
